11af0d8703319ba1cf73e7d3d4d664902788672f
zhangpanyi <zhangpanyi@live.com>
2015-06-23 17:48:41 +0800

    update

---
+++ Classes/Backend.cpp
@@ -0,0 +1,187 @@
+﻿#include "Backend.h"
+#include <random>
+
+
+Backend::Backend()
+	: initialized_(false)
+{
+
+}
+
+void Backend::Clear()
+{
+	elements_.clear();
+	config_.type = 0;
+	config_.width = 0;
+	config_.height = 0;
+	config_.data.clear();
+	initialized_ = false;
+}
+
+// 设置地图
+void Backend::SetMap(const MapConfig &config)
+{
+	if (config.data.size() == config.width * config.height)
+	{
+		config_ = config;
+		elements_.clear();
+		ReGeneration();
+		souch_scope_.init();
+		initialized_ = true;
+	}
+	else
+	{
+		throw std::runtime_error("Invalid map configuration!");
+	}
+}
+
+// 随机元素类型
+int Backend::RandElementType() const
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(1, config_.type);
+	return dis(gen);
+}
+
+// 重新生成地图
+void Backend::ReGeneration()
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	for (int idx = 0; idx < config_.width * config_.height; ++idx)
+	{
+		elements_.push_back(config_.data[idx] ? RandElementType() : 0);
+	}
+}
+
+// 有效元素索引
+bool Backend::IsValidElements(const MapIndex &index)
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	if (index.col >= 0 && index.row >= 0 && index.col < config_.width && index.row < config_.height)
+	{
+		return elements_[index.row * config_.width + index.col] != 0;
+	}
+	return false;
+}
+
+// 遍历地图
+void Backend::VisitMap(const VisitCallback &step)
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	for (int idx = 0; idx < config_.width * config_.height; ++idx)
+	{
+		step(MapIndex(idx / config_.width, idx % config_.width), elements_[idx]);
+	}
+}
+
+// 交换元素
+void Backend::SwapElement(const MapIndex &a, const MapIndex &b)
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	if (!IsValidElements(a) || !IsValidElements(b))
+	{
+		throw std::runtime_error("invalid element index!");
+	}
+	std::swap(elements_[a.row * config_.width + a.col], elements_[b.row * config_.width + b.col]);
+}
+
+// 是否可消除
+bool Backend::IsCanEliminate(const MapIndex &index, std::set<MapIndex> &out)
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	out.clear();
+	if (IsValidElements(index))
+	{
+		std::set<MapIndex> elements_set;
+		const int type = elements_[index.row * config_.width + index.col];
+
+		// 横向遍历
+		for (int col = 0; col < config_.width; ++col)
+		{
+			const int idx = index.row * config_.width + col;
+			if (elements_[idx] == type)
+			{
+				elements_set.insert(MapIndex(index.row, col));
+			}
+			else
+			{
+				if (elements_set.size() >= 3 && (elements_set.find(index) != elements_set.end()))
+				{
+					break;
+				}
+				elements_set.clear();
+			}
+		}
+		if (elements_set.size() < 3) elements_set.clear();
+
+		// 纵向遍历
+		for (int row = 0; row < config_.height; ++row)
+		{
+			const int idx = row * config_.width + index.col;
+			if (elements_[idx] == type)
+			{
+				out.insert(MapIndex(row, index.col));
+			}
+			else
+			{
+				if (out.size() >= 3 && (out.find(index) != out.end()))
+				{
+					break;
+				}
+				out.clear();
+			}
+		}
+		if (out.size() < 3) out.clear();
+
+		for (auto &idx : elements_set)
+		{
+			out.insert(idx);
+		}
+
+		return out.empty() == false;
+	}
+	return false;
+}
+
+// 执行消除
+void Backend::DoEliminate(std::set<MapIndex> &in_elements, const EliminateCallback &step)
+{
+	if (!initialized_)
+	{
+		throw std::runtime_error("map configuration is not set!");
+	}
+
+	for (auto &index : in_elements)
+	{
+#ifdef _DEBUG
+		if (!IsValidElements(index))
+		{
+			throw std::runtime_error("invalid element index!");
+		}
+#endif
+		elements_[index.row * config_.width + index.col] = 0;
+		souch_scope_.update(index);
+		step(index);
+	}
+}
---
+++ Classes/Backend.h
@@ -0,0 +1,103 @@
+﻿#pragma once
+
+#include <set>
+#include <cstdint>
+#include <functional>
+#include "Types.h"
+
+class Backend
+{
+	struct Scope
+	{
+		int min_row;
+		int min_col;
+		int max_row;
+		int max_col;
+
+		void init()
+		{
+			min_row = min_col = max_row = max_col = -1;
+		}
+
+		void update(const MapIndex &index)
+		{
+			min_row = min_row == -1 ? index.row : index.row < min_row ? index.row : min_row;
+			min_col = min_col == -1 ? index.col : index.col < min_col ? index.col : min_col;
+			max_row = max_row == -1 ? index.row : index.row > max_row ? index.row : max_row;
+			max_col = max_col == -1 ? index.col : index.col > max_col ? index.col : max_col;
+		}
+	};
+
+	typedef std::function<void(const MapIndex &index)> EliminateCallback;
+	typedef std::function<void(const MapIndex &index, int type)> VisitCallback;
+
+public:
+	Backend();
+	~Backend() = default;
+
+public:
+	void Clear();
+
+	/**
+	 * 重新生成地图
+	 */
+	void ReGeneration();
+
+	/**
+	 * 设置地图
+	 * @param config 地图配置
+	 */
+	void SetMap(const MapConfig &config);
+
+	/**
+	 * 有效元素索引
+	 * @param 元素索引
+	 */
+	bool IsValidElements(const MapIndex &index);
+
+	/**
+	 * 遍历地图
+	 * @param step 回调函数
+	 * VisitCallback type=0表示此索引是地板
+	 */
+	void VisitMap(const VisitCallback &step);
+
+	/**
+	 * 交换元素
+	 * @param a 元素a索引
+	 * @param b 元素b索引
+	 */
+	void SwapElement(const MapIndex &a, const MapIndex &b);
+
+public:
+	/**
+	 * 是否可消除
+	 * @param index 焦点索引
+	 * @param out 可消除索引集合
+	 * @return bool
+	 */
+	virtual bool IsCanEliminate(const MapIndex &index, std::set<MapIndex> &out);
+
+	/**
+	 * 执行消除
+	 * @param 将被消除的元素集合
+	 */
+	virtual void DoEliminate(std::set<MapIndex> &in_elements, const EliminateCallback &step);
+
+	/**
+	 * 补充元素(用于执行完所有消除之后调用)
+	 */
+	virtual void SuppliesElements();
+
+private:
+	/**
+	 * 随机元素类型
+	 */
+	int RandElementType() const;
+
+private:
+	bool initialized_;
+	MapConfig config_;
+	Scope souch_scope_;
+	std::vector<int> elements_;
+};
